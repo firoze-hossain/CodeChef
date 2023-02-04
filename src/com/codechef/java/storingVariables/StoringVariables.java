f7629ce5512939e07f5360310561fb61d4c7fd9d
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-04 19:43:45 +0600

    Storing Variables

---
+++ src/com/codechef/java/storingVariables/StoringVariables.java
@@ -0,0 +1,13 @@
+package com.codechef.java.storingVariables;
+
+public class StoringVariables {
+    public static void main(String[] args) {
+        int width = 20;
+        int length = 30;
+        System.out.println(length * width);
+        System.out.println(2 * (length * width));
+        width = 15;
+        System.out.println(length * width);
+        System.out.println(2 * (length * width));
+    }
+}
--- src/com/codechef/xJumps/XJumps.java
+++
@@ -1,4 +0,0 @@
-package com.codechef.xJumps;
-
-public class XJumps {
-}
