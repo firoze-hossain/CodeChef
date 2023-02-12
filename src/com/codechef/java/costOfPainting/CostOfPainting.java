99245537c53c16ecbe50dc495799ac3e0958ac5a
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-12 22:29:19 +0600

    Cost of Painting

---
+++ src/com/codechef/java/costOfPainting/CostOfPainting.java
@@ -0,0 +1,11 @@
+package com.codechef.java.costOfPainting;
+
+public class CostOfPainting {
+    public static void main(String[] args) {
+        int s = 14;
+        int area = s * s;
+        int cost = area * 7;
+        System.out.println(area);
+        System.out.print(cost + "$");
+    }
+}
