c14f2ace546ab3e6bf412eb0b9bf289bd8a11b83
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-13 23:40:14 +0600

    Accessing characters of a String

---
+++ src/com/codechef/java/accessingCharactersOfAString/AccessingCharactersOfAString.java
@@ -0,0 +1,9 @@
+package com.codechef.java.accessingCharactersOfAString;
+
+public class AccessingCharactersOfAString {
+    public static void main(String[] args) {
+        String myString = "Programming";
+        System.out.println(myString.charAt(2));
+        System.out.print(myString.charAt(4));
+    }
+}
