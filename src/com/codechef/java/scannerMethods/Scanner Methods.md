9dd260e094d31405e0b259602093afb7235d1462
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-14 19:02:33 +0600

    Scanner Methods

---
+++ src/com/codechef/java/scannerMethods/Scanner Methods.md
@@ -0,0 +1,1 @@
+https://www.codechef.com/learn/BJ00LP03/problems/USRJ2
---
+++ src/com/codechef/java/scannerMethods/ScannerMethods.java
@@ -0,0 +1,11 @@
+package com.codechef.java.scannerMethods;
+
+import java.util.Scanner;
+
+public class ScannerMethods {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int side = sc.nextInt();
+        System.out.println(side * side);
+    }
+}
