f11e6c6b13dd0774404b8568ecf84cdce0222eaf
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-22 22:46:45 +0600

    Changing array elemets

---
+++ out/production/Codechef/com/codechef/java/changingArrayElements/Changing array elements.md
@@ -0,0 +1,1 @@
+https://www.codechef.com/learn/BJ00LP05/problems/ARRYJ3
---
+++ src/com/codechef/java/changingArrayElements/ChangingArrayElements.java
@@ -0,0 +1,9 @@
+package com.codechef.java.changingArrayElements;
+
+public class ChangingArrayElements {
+    public static void main(String[] args) {
+        String[] mnts = {"January", "February", "December", "April"};
+        mnts[2] = "March";
+        System.out.println(mnts[2]);
+    }
+}
