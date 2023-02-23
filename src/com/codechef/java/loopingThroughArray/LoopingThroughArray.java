458ba7c3972314321656c474fcd6fa0c677f1b74
Md. Firoze Hossain <firoze.hossain@leads-bd.com>
2023-02-23 15:43:09 +0600

    Looping through Array

---
+++ src/com/codechef/java/loopingThroughArray/LoopingThroughArray.java
@@ -0,0 +1,10 @@
+package com.codechef.java.loopingThroughArray;
+
+public class LoopingThroughArray {
+    public static void main(String[] args) {
+        String[] x = {"C", "O", "D", "I", "N", "G"};
+        for (int i = 0; i < 6; i++) {
+            System.out.println(i + " = " + x[i]);
+        }
+    }
+}
