a523494ce2e981dbe46b054c72d5f001879ec03d
Md. Firoze Hossain <firoze.hossain@leads-bd.com>
2023-02-23 12:51:49 +0600

    Break Statement

---
+++ src/com/codechef/java/breakStatement/BreakStatement.java
@@ -0,0 +1,12 @@
+package com.codechef.java.breakStatement;
+
+public class BreakStatement {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 15; i++) {
+            if (i == 13) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
