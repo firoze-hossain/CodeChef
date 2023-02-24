e3d90ae78cd4ebf91ee0eff18164d461149e3ac7
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-24 21:15:50 +0600

    How to accept integer inputs

---
+++ src/com/codechef/solveProblemUsingJava/acceptIntegerInputs/AcceptIntegerInputs.java
@@ -0,0 +1,11 @@
+package com.codechef.solveProblemUsingJava.acceptIntegerInputs;
+
+import java.util.Scanner;
+
+public class AcceptIntegerInputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N);
+    }
+}
