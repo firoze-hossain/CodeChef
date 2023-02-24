2125541522ea7f4993083b9fb2424054c5233e47
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-25 00:57:14 +0600

    How to accept multiple integers on separate lines

---
+++ src/com/codechef/solveProblemUsingJava/acceptMultipleIntegers/AcceptMultipleIntegers.java
@@ -0,0 +1,15 @@
+package com.codechef.solveProblemUsingJava.acceptMultipleIntegers;
+
+import java.util.Scanner;
+
+public class AcceptMultipleIntegers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+        int E = sc.nextInt();
+        System.out.print(A + " " + B + " " + C + " " + D + " " + E);
+    }
+}
