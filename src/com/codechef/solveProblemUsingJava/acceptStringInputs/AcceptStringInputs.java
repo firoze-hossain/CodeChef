a2f7b5187f95ac8aefb91d76644426a868f3ce4f
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-25 01:21:00 +0600

    How to accept string inputs

---
+++ src/com/codechef/solveProblemUsingJava/acceptStringInputs/AcceptStringInputs.java
@@ -0,0 +1,15 @@
+package com.codechef.solveProblemUsingJava.acceptStringInputs;
+
+import java.util.Scanner;
+
+public class AcceptStringInputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        String C = sc.next();
+        String D = sc.next();
+        String E = sc.next();
+        System.out.print(A + " " + B + " " + C + " " + D + " " + E);
+    }
+}
