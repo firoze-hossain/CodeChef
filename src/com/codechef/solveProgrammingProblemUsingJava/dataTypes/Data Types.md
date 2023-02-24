f68d5f8638c5983db460377401b9faefa1189cda
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-24 08:59:17 +0600

    Data Types

---
+++ src/com/codechef/solveProgrammingProblemUsingJava/dataTypes/Data Types.md
@@ -0,0 +1,1 @@
+https://www.codechef.com/learn/BJ00BP01/problems/GSJ02
---
+++ src/com/codechef/solveProgrammingProblemUsingJava/dataTypes/DataTypes.java
@@ -0,0 +1,14 @@
+package com.codechef.solveProgrammingProblemUsingJava.dataTypes;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int a = -6;
+        System.out.println(a);
+        double d = 4.45;
+        System.out.println(d);
+        String s = "Firoze";
+        System.out.println(s);
+        String[] arr = {"01", "Hossain", "Software Engineer"};
+        System.out.println(arr[2]);
+    }
+}
