5408fb04c584360746268cdd4713571b393918a2
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-25 17:46:22 +0600

    What are test cases

---
+++ src/com/codechef/solveProblemUsingJava/testCases/TestCases.java
@@ -0,0 +1,15 @@
+package com.codechef.solveProblemUsingJava.testCases;
+
+import java.util.Scanner;
+
+public class TestCases {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        String C = sc.next();
+        String D = sc.next();
+        String E = sc.next();
+        System.out.print(A + "\n" + B + "\n" + C + "\n" + D + "\n" + E);
+    }
+}
