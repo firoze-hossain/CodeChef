487a87de747f84800816eb8b18aade7f3f7bbb3d
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-25 23:18:51 +0600

    Test cases with multiple lines of input

---
+++ src/com/codechef/solveProblemUsingJava/testCases2/TestCases2.java
@@ -0,0 +1,18 @@
+package com.codechef.solveProblemUsingJava.testCases2;
+
+import java.util.Scanner;
+
+public class TestCases2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+            int e = sc.nextInt();
+            System.out.println(a + " " + b + " " + c + " " + d + " " + e);
+        }
+    }
+}
