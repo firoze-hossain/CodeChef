1eaf3126a50e81c1bcff9fef27d03fb34685e934
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-25 18:30:20 +0600

    What are test cases1

---
+++ src/com/codechef/solveProblemUsingJava/testCases1/TestCases1.java
@@ -0,0 +1,14 @@
+package com.codechef.solveProblemUsingJava.testCases1;
+
+import java.util.Scanner;
+
+public class TestCases1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            System.out.println(n);
+        }
+    }
+}
