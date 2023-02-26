662903dd806684144dcf2d93b0c218b9d927498b
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-26 23:37:54 +0600

    Debug this code - Why is this code incorrect

---
+++ src/com/codechef/solveProblemUsingJava/debugCode/DebugCode.java
@@ -0,0 +1,16 @@
+package com.codechef.solveProblemUsingJava.debugCode;
+
+import java.util.Scanner;
+
+public class DebugCode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            System.out.println(n * 2);
+
+
+        }
+    }
+}
