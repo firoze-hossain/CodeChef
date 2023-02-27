848609e44659f5cefa4d483c0d53b2adea85951d
Md. Firoze Hossain <firoze.hossain@leads-bd.com>
2023-02-27 10:53:33 +0600

    Practice problem - Tuition fees

---
+++ src/com/codechef/solveProblemUsingJava/tuitionFees/TuitionFees.java
@@ -0,0 +1,15 @@
+package com.codechef.solveProblemUsingJava.tuitionFees;
+
+import java.util.Scanner;
+
+public class TuitionFees {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(x * y);
+        }
+    }
+}
