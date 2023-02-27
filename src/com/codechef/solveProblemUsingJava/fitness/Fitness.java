da8906076bed13a7e5bd9e54f9062b2f410cd50e
Md. Firoze Hossain <firoze.hossain@leads-bd.com>
2023-02-27 14:24:26 +0600

    Practice problem - Fitness

---
+++ src/com/codechef/solveProblemUsingJava/fitness/Fitness.java
@@ -0,0 +1,14 @@
+package com.codechef.solveProblemUsingJava.fitness;
+
+import java.util.Scanner;
+
+public class Fitness {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int x = sc.nextInt();
+            System.out.println(2 * x * 5);
+        }
+    }
+}
