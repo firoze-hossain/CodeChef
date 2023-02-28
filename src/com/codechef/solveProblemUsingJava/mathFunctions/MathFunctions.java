42997d3c9997f384c79984f45fc4dfddc1db2470
Md. Firoze Hossain <firoze.hossain@leads-bd.com>
2023-02-28 16:23:23 +0600

    Some useful math functions

---
+++ src/com/codechef/solveProblemUsingJava/mathFunctions/MathFunctions.java
@@ -0,0 +1,18 @@
+package com.codechef.solveProblemUsingJava.mathFunctions;
+
+import java.util.Scanner;
+
+public class MathFunctions {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 36;
+        System.out.println(Math.min(a, b));
+        System.out.println(Math.max(a, b));
+        System.out.println(Math.abs(b - a));
+        int c = 6;
+        int d = 8;
+        System.out.println(b % c);
+        System.out.println(b % d);
+
+    }
+}
