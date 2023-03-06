f0cdef44878214eb3ea1a1cb627348f099ee32ed
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-06 20:10:12 +0600

    Problem (Make Avg) - Solve sub-components

---
+++ src/com/codechef/solveProblemUsingJava/problemMakeAvg/ProblemMakeAvg.java
@@ -0,0 +1,15 @@
+package com.codechef.solveProblemUsingJava.problemMakeAvg;
+
+public class ProblemMakeAvg {
+    public static void main(String[] args) {
+        int a = 15;
+        int c = 19;
+        int b = (a + c) / 2;
+        System.out.println(b);
+        a = 16;
+        c = 18;
+        b = (a + c) / 2;
+        System.out.println(b);
+
+    }
+}
