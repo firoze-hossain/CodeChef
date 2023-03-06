9d33393ab43c757efd0494b1b952410602cdd6bd
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-06 18:32:22 +0600

    Problem (Make Avg) - Generate examples

---
+++ src/com/codechef/solveProblemUsingJava/makeAvgExam/MakeAvgExam.java
@@ -0,0 +1,10 @@
+package com.codechef.solveProblemUsingJava.makeAvgExam;
+
+public class MakeAvgExam {
+    public static void main(String[] args) {
+        System.out.println("For A = 3 and C = 4, the output is " + "-1");
+        System.out.println("For A = 3 and C = 5, the output is " + "4");
+        System.out.println("For A = 2 and C = 4, the output is " + "3");
+        System.out.println("For A = 2 and C = 5, the output is " + "-1");
+    }
+}
