ca38ba4588cf770a371df33aba9f69f50383a094
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-06 22:53:07 +0600

    Problem(Farm animals) - Generate examples

---
+++ src/com/codechef/solveProblemUsingJava/farmAnimalsGenExam/FarmAnimalsGenExam.java
@@ -0,0 +1,25 @@
+package com.codechef.solveProblemUsingJava.farmAnimalsGenExam;
+
+public class FarmAnimalsGenExam {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 4;
+        int z = 12;
+        System.out.println("The animal on the farm is ANY");
+
+        x = 3;
+        y = 5;
+        z = 13;
+        System.out.println("The animal on the farm is NONE");
+
+        x = 3;
+        y = 5;
+        z = 10;
+        System.out.println("The animal on the farm is DUCK");
+
+        x = 3;
+        y = 5;
+        z = 9;
+        System.out.println("The animal on the farm is CHICKEN");
+    }
+}
