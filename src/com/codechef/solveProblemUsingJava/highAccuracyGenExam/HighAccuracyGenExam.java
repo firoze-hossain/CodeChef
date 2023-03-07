fce88a1bf8702a0223978d0eb59c11abe24887d5
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-07 23:27:18 +0600

    Problem 3 (High Accuracy) - Generate examples

---
+++ src/com/codechef/solveProblemUsingJava/highAccuracyGenExam/HighAccuracyGenExam.java
@@ -0,0 +1,17 @@
+package com.codechef.solveProblemUsingJava.highAccuracyGenExam;
+
+public class HighAccuracyGenExam {
+    public static void main(String[] args) {
+        int x = 18;
+        System.out.println("minimum number of problems Chef marked incorrect are 0");
+
+        x = 19;
+        System.out.println("minimum number of problems Chef marked incorrect are 2");
+
+        x = 20;
+        System.out.println("minimum number of problems Chef marked incorrect are 1");
+
+        x = 7;
+        System.out.println("minimum number of problems Chef marked incorrect are 2");
+    }
+}
