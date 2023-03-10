7cc4c40aed545a8b2e58beddd5e33e00a057456c
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-10 23:47:19 +0600

    Problem (MOVIE2X) - Generate examples

---
+++ src/com/codechef/solveProblemUsingJava/problemMOVIE2X/ProblemMOVIE2X.java
@@ -0,0 +1,21 @@
+package com.codechef.solveProblemUsingJava.problemMOVIE2X;
+
+public class ProblemMOVIE2X {
+    public static void main(String[] args) {
+        int x = 90;
+        int y = 40;
+        System.out.println("Given X and Y, the total watchtime of the movie is 70");
+
+        x = 80;
+        y = 80;
+        System.out.println("Given X and Y, the total watchtime of the movie is 40");
+
+        x = 60;
+        y = 10;
+        System.out.println("Given X and Y, the total watchtime of the movie is 55");
+
+        x = 60;
+        y = 50;
+        System.out.println("Given X and Y, the total watchtime of the movie is 35");
+    }
+}
