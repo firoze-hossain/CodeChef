98f8886f552f8447be9c1ae425b3acb86427ba6d
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-11 00:26:32 +0600

    Problem (Flip cards) - Generate examples

---
+++ src/com/codechef/solveProblemUsingJava/flipCardsProb/FlipCardsProb.java
@@ -0,0 +1,23 @@
+package com.codechef.solveProblemUsingJava.flipCardsProb;
+
+public class FlipCardsProb {
+    public static void main (String[] args)
+    {
+        int n = 10;
+        int x = 3;
+        System.out.println("The number of cards we need to flip is 3");
+
+        n = 10;
+        x = 7;
+        System.out.println("The number of cards we need to flip is 3");
+
+        n = 10;
+        x = 4;
+        System.out.println("The number of cards we need to flip is 4");
+
+        n = 10;
+        x = 6;
+        System.out.println("The number of cards we need to flip is 4");
+
+    }
+}
