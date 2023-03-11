1602785e6340b0b3a4d1c5fa2016bc69b15a364e
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-11 19:44:50 +0600

    Problem (Shoes) - Generate examples

---
+++ src/com/codechef/solveProblemUsingJava/problemShoesGen/Problem (Shoes) - Generate examples .md
@@ -0,0 +1,1 @@
+https://www.codechef.com/learn/BJ00BP14/problems/LBJ08B
---
+++ src/com/codechef/solveProblemUsingJava/problemShoesGen/ProblemShoesGen.java
@@ -0,0 +1,17 @@
+package com.codechef.solveProblemUsingJava.problemShoesGen;
+
+public class ProblemShoesGen {
+    public static void main(String[] args) {
+        int n = 10;
+        int m = 10;
+        System.out.println("The number of extra shoes that Chef has to buy is 10");
+
+        n = 5;
+        m = 10;
+        System.out.println("The number of extra shoes that Chef has to buy is 5");
+
+        n = 15;
+        m = 10;
+        System.out.println("The number of extra shoes that Chef has to buy is 20");
+    }
+}
