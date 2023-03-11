5b33109cda024d98304bcc4377442f24cb5b7c67
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-12 00:46:56 +0600

    Problem (X Jumps) - Generate examples

---
+++ src/com/codechef/solveProblemUsingJava/problemXJumpsGen/ProblemXJumpsGen.java
@@ -0,0 +1,17 @@
+package com.codechef.solveProblemUsingJava.problemXJumpsGen;
+
+public class ProblemXJumpsGen {
+    public static void main(String[] args) {
+        int x = 18;
+        int y = 3;
+        System.out.println("Minimum moves required to reach stair " + x + " is 6");
+
+        x = 19;
+        y = 3;
+        System.out.println("Minimum moves required to reach stair " + x + " is 7");
+
+        x = 2;
+        y = 3;
+        System.out.println("Minimum moves required to reach stair " + x + " is 2");
+    }
+}
