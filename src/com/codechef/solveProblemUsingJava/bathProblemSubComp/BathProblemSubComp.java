d83df2a45150128ee747a30a98a9f025d323b4df
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-11 09:35:17 +0600

    Problem (Bath) - Solve sub-components

---
+++ src/com/codechef/solveProblemUsingJava/bathProblemSubComp/BathProblemSubComp.java
@@ -0,0 +1,23 @@
+package com.codechef.solveProblemUsingJava.bathProblemSubComp;
+
+public class BathProblemSubComp {
+    public static void main(String[] args) {
+        int x = 15;
+        int y = 1;
+        int buckets_filled = x % y;
+        if (buckets_filled == 0 && buckets_filled != 1) {
+            buckets_filled = x / y;
+            int people = buckets_filled / 2;
+            System.out.println(people);
+        }
+        x = 15;
+        y = 3;
+
+        buckets_filled = x % y;
+        if (buckets_filled == 0 && buckets_filled != 1) {
+            buckets_filled = x / y;
+            int people = buckets_filled / 2;
+            System.out.println(people);
+        }
+    }
+}
