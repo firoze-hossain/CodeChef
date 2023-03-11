c3377182ec06c232cdc06d315a351469db2a4e80
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-11 08:52:28 +0600

    Problem (Bath) - Generate examples

---
+++ src/com/codechef/solveProblemUsingJava/bathProblem/BathProblem.java
@@ -0,0 +1,21 @@
+package com.codechef.solveProblemUsingJava.bathProblem;
+
+public class BathProblem {
+    public static void main(String[] args) {
+        int x = 15;
+        int y = 1;
+        System.out.println("The maximum number of people that can take a bath is 7");
+
+        x = 15;
+        y = 2;
+        System.out.println("The maximum number of people that can take a bath is 3");
+
+        x = 15;
+        y = 3;
+        System.out.println("The maximum number of people that can take a bath is 2");
+
+        x = 15;
+        y = 4;
+        System.out.println("The maximum number of people that can take a bath is 1");
+    }
+}
