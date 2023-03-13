30c04240a0021623902c376a5320f7d32531b28c
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-13 15:03:32 +0600

    Practice problem - Candy Distribution

---
+++ src/com/codechef/solveProblemUsingJava/candyDistribution/CandyDistribution.java
@@ -0,0 +1,19 @@
+package com.codechef.solveProblemUsingJava.candyDistribution;
+
+import java.util.Scanner;
+
+public class CandyDistribution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            if (n % m == 0 && (n / m) % 2 == 0) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
