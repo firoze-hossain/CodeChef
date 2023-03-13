b0a2e61f8f0df72f7a8505392ac467176997d049
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-13 23:24:12 +0600

    Practice problem - Minimum number of coins

---
+++ src/com/codechef/solveProblemUsingJava/minNumOfCoins/MinNumOfCoins.java
@@ -0,0 +1,20 @@
+package com.codechef.solveProblemUsingJava.minNumOfCoins;
+
+import java.util.Scanner;
+
+public class MinNumOfCoins {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            if (x % 10 == 0) {
+                System.out.println(x / 10);
+            } else if (x % 5 == 0) {
+                System.out.println((x / 10) + 1);
+            } else {
+                System.out.println(-1);
+            }
+        }
+    }
+}
