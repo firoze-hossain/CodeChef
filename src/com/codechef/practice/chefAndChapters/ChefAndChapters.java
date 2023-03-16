d0ebe0ddacbd42974fe6ebf37707f5995a60f560
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-17 00:06:20 +0600

    Chef and Chapters

---
+++ src/com/codechef/practice/chefAndChapters/ChefAndChapters.java
@@ -0,0 +1,17 @@
+package com.codechef.practice.chefAndChapters;
+
+import java.util.Scanner;
+
+public class ChefAndChapters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+            System.out.println(x * y * z);
+        }
+    }
+
+}
