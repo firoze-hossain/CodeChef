5b7bf6f67badabcbc132bcecc3995ba43695802d
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-03-17 01:07:24 +0600

    Alice and Marks

---
+++ src/com/codechef/practice/aliceAndMarks/AliceAndMarks.java
@@ -0,0 +1,18 @@
+package com.codechef.practice.aliceAndMarks;
+
+import java.util.Scanner;
+
+public class AliceAndMarks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if (x >= 2 * y) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+    }
+
+}
