adbac940792f6d7447a8b6e0e8640886588b62a3
Md. Firoze Hossain <firoze.hossain@leads-bd.com>
2023-03-19 09:58:25 +0600

    Chef and Instant Noodles

---
+++ src/com/codechef/practice/chefAndInstantNoodles/ChefAndInstantNoodles.java
@@ -0,0 +1,12 @@
+package com.codechef.practice.chefAndInstantNoodles;
+
+import java.util.Scanner;
+
+public class ChefAndInstantNoodles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(x * y);
+    }
+}
