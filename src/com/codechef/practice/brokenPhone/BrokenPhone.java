f8fba603b76d33c6f6f8dab720d3f8366f219b18
Md. Firoze Hossain <firoze.hossain@leads-bd.com>
2023-03-19 09:00:22 +0600

    Broken Phone

---
+++ src/com/codechef/practice/brokenPhone/BrokenPhone.java
@@ -0,0 +1,21 @@
+package com.codechef.practice.brokenPhone;
+
+import java.util.Scanner;
+
+public class BrokenPhone {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if (x > y) {
+                System.out.println("NEW PHONE");
+            } else if (y > x) {
+                System.out.println("REPAIR");
+            } else {
+                System.out.println("ANY");
+            }
+        }
+    }
+}
