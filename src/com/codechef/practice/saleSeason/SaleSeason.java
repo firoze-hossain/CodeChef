1c676201ee4b006abcff1a244fb4db6b83d94118
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-04-09 22:29:41 +0600

    Sale Season

---
+++ src/com/codechef/practice/saleSeason/SaleSeason.java
@@ -0,0 +1,22 @@
+package com.codechef.practice.saleSeason;
+
+import java.util.Scanner;
+
+public class SaleSeason {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            if (x <= 100) {
+                System.out.println(x);
+            } else if (x > 100 && x <= 1000) {
+                System.out.println(x - 25);
+            } else if (x > 1000 && x <= 5000) {
+                System.out.println(x - 100);
+            } else {
+                System.out.println(x - 500);
+            }
+        }
+    }
+}
