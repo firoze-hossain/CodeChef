8d62b2060444886942af4deb233a9c2da5f2aa4a
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-04-23 12:07:02 +0600

    CodeChef Learn Problem Solving

---
+++ src/com/codechef/practice/codeChefLearnProblemSolving/CodeChefLearnProblemSolving.java
@@ -0,0 +1,11 @@
+package com.codechef.practice.codeChefLearnProblemSolving;
+
+import java.util.Scanner;
+
+public class CodeChefLearnProblemSolving {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n * 2);
+    }
+}
