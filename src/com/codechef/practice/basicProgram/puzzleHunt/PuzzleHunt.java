1076d330b4696aef7342d2c89a23e1fbc9b3c2f5
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-08-26 19:22:50 +0600

    Puzzle Hunt

---
+++ src/com/codechef/practice/basicProgram/puzzleHunt/PuzzleHunt.java
@@ -0,0 +1,15 @@
+package com.codechef.practice.basicProgram.puzzleHunt;
+
+import java.util.Scanner;
+
+public class PuzzleHunt {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n >= 6 && n <= 8) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
