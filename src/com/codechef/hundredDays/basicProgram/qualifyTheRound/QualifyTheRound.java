08984f9faacca3487d195b109081983798032864
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-09-05 19:21:05 +0600

    Qualify the round

---
+++ src/com/codechef/hundredDays/basicProgram/qualifyTheRound/QualifyTheRound.java
@@ -0,0 +1,20 @@
+package com.codechef.hundredDays.basicProgram.qualifyTheRound;
+
+import java.util.Scanner;
+
+public class QualifyTheRound {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a + b * 2 >= x) {
+                System.out.println("Qualify");
+            } else {
+                System.out.println("NotQualify");
+            }
+        }
+    }
+}
