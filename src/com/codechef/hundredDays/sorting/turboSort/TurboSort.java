fa828fe02242d3af0e8a77397fa7a1aa57482389
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-09-09 00:37:26 +0600

    Turbo Sort

---
+++ src/com/codechef/hundredDays/sorting/turboSort/TurboSort.java
@@ -0,0 +1,19 @@
+package com.codechef.hundredDays.sorting.turboSort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TurboSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
