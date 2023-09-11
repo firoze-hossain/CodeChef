429059fb9a127bffd25825eef1eaf93ad8d23d60
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-09-12 01:03:28 +0600

    Sum OR Difference

---
+++ src/com/codechef/hundredDays/basicProgram/sumOrDifference/SumOrDifference.java
@@ -0,0 +1,16 @@
+package com.codechef.hundredDays.basicProgram.sumOrDifference;
+
+import java.util.Scanner;
+
+public class SumOrDifference {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        if (n1 > n2) {
+            System.out.println(n1 - n2);
+        } else {
+            System.out.println(n1 + n2);
+        }
+    }
+}
