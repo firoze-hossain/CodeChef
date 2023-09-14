74dcbf7cfc9e36d68738580531a90117c46f028d
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-09-14 22:44:48 +0600

    Air Quality Index

---
+++ src/com/codechef/hundredDays/basicProgram/airQualityIndex/AirQualityIndex.java
@@ -0,0 +1,15 @@
+package com.codechef.hundredDays.basicProgram.airQualityIndex;
+
+import java.util.Scanner;
+
+public class AirQualityIndex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (x >= 100) {
+            System.out.println("No");
+        } else {
+            System.out.println("Yes");
+        }
+    }
+}
