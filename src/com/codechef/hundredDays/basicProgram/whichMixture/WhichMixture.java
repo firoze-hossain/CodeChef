79698ba4956b9c723cf841251a1a87cbbf1b4d49
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-09-16 16:48:06 +0600

    Which Mixture

---
+++ src/com/codechef/hundredDays/basicProgram/whichMixture/WhichMixture.java
@@ -0,0 +1,21 @@
+package com.codechef.hundredDays.basicProgram.whichMixture;
+
+import java.util.Scanner;
+
+public class WhichMixture {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a > 0 && b > 0) {
+                System.out.println("Solution");
+            } else if (b == 0) {
+                System.out.println("Solid");
+            } else {
+                System.out.println("Liquid");
+            }
+        }
+    }
+}
