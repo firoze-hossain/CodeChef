798b301b2721380ac1ac40e401dcd6199df8f936
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-09-18 21:07:16 +0600

    Watching Movies at 2x

---
+++ src/com/codechef/hundredDays/basicProgram/watchingMoviesAt2x/WatchingMoviesAt2x.java
@@ -0,0 +1,12 @@
+package com.codechef.hundredDays.basicProgram.watchingMoviesAt2x;
+
+import java.util.Scanner;
+
+public class WatchingMoviesAt2x {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println((x - y / 2));
+    }
+}
