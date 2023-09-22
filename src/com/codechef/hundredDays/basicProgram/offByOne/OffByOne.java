79652e4396ea740fda54e2e68c00e102b07c15fa
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-09-22 11:16:49 +0600

    Off By One

---
+++ src/com/codechef/hundredDays/basicProgram/offByOne/OffByOne.java
@@ -0,0 +1,12 @@
+package com.codechef.hundredDays.basicProgram.offByOne;
+
+import java.util.Scanner;
+
+public class OffByOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b + "1");
+    }
+}
