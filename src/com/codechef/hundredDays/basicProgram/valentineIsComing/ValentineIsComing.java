233a257592277a1da555994cc6ee406332475fda
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-10-13 12:36:53 +0600

    Valentine is Coming

---
+++ src/com/codechef/hundredDays/basicProgram/valentineIsComing/ValentineIsComing.java
@@ -0,0 +1,16 @@
+package com.codechef.hundredDays.basicProgram.valentineIsComing;
+
+import java.util.Scanner;
+
+public class ValentineIsComing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(x / y);
+        }
+    }
+
+}
